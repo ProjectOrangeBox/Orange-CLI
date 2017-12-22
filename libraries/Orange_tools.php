5ac4feaf39d4162bda173c18286fb268da6ada1e
Don Myers <donmyers@quadratec.com>
2017-12-22 14:53:58 -0500

    updates

--- libraries/Orange_tools.php
+++ libraries/Orange_tools.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class Orange_tools {
 	public function __construct() {
@@ -6,55 +6,63 @@ class Orange_tools {
 	}
 
 	public function git_status($output_as='none',$table_template=null) {
-		/* search all the folder under root for .git/HEAD */		
+		/* search all the folder under root for .git/HEAD */
 		exec('find "'.ROOTPATH.'" -name HEAD',$output);
 
 		$array = false;
 
 		foreach ($output as $o) {
+			$dirname = dirname($o);
+
 			if (strpos($o,'/.git/HEAD') !== false) {
 				$stringfromfile = file($o);
-				
+
 				$firstLine = $stringfromfile[0]; //get the string from the array
-				
+
 				$explodedstring = explode("/", $firstLine, 3); //separate out by the "/" in the string
-				
+
 				$branchname = $explodedstring[2]; //get the one that is always the branch name
 
 				$key = str_replace('/.git/HEAD','',$o);
 
 				$sections = explode('/',$key);
-				
+
 				$key = end($sections);
 
-				$array[$key] = $branchname;
+				$lines = file($dirname.'/FETCH_HEAD');
+
+				$firstline = $lines[0];
+
+				$segs  = explode(' ',str_replace([' ',chr(9)],' ',$firstline));
+
+				$array[$key] = ['branch'=>$branchname,'commit'=>$segs[0]];
 			}
 		}
-		
+
 		$table = [];
-	
+
 		if ($array) {
-			$table[] = ['Package','Branch'];
-			
+			$table[] = ['Package','Branch','Hash'];
+
 			foreach ($array as $key=>$val) {
-				$table[] = [$key,$val];
+				$table[] = [$key,$val['branch'],$val['commit']];
 			}
-		
+
 			$responds = '';
-			
+
 			switch($output_as) {
 				case 'cli':
 					foreach ($table as $row) {
-						$responds .= trim(str_pad($row[0],32,' ',STR_PAD_RIGHT).$row[1]).chr(10);	
+						$responds .= trim(str_pad($row[0],32,' ',STR_PAD_RIGHT).str_pad($row[1],16,' ',STR_PAD_RIGHT),str_pad($row[2],36,' ',STR_PAD_RIGHT)).chr(10);
 					}
 				break;
 				case 'html':
 					ci()->load->library('table');
-					
+
 					if ($table_template) {
 						ci()->table->set_template($table_template);
 					}
-					
+
 					$responds .= ci()->table->generate($table);
 				break;
 				default:
@@ -76,36 +84,36 @@ class Orange_tools {
 
 	public function fix_acl() {
 		delete_cache_by_tags('acl');
-		
+
 		ci()->o_permission_model->administrator_refresh();
 	}
 
 	public function clear_cache() {
 		$http = config('config.base_url');
 		$md5 = md5(config('config.encryption_key').config('config.base_url'));
-	
+
 		return file_get_contents(rtrim($http,'/').'/cli/fix/cache/'.$md5);
 	}
-	
+
 	public function clear_cache_process($key) {
 		$md5 = md5(config('config.encryption_key').config('config.base_url'));
-		
+
 		$success = 'failed';
-		
+
 		if ($key === $md5) {
 			if (ci()->cache->clean() === true) {
 				$success = 'success';
 			}
 		}
-		
+
 		return $success;
 	}
 
 	public function fix_file_permissions() {
 		$composer_obj = $this->get_composer_object(ROOTPATH . '/composer.json');
-	
+
 		ci()->console->output('Setting the Default Permissions');
-		
+
 		/* files */
 		$cli = 'find "'.ROOTPATH.'" -type f | xargs chmod 664';
 
@@ -118,24 +126,24 @@ class Orange_tools {
 
 		/* change the others back */
 		ci()->console->output('Changes Cache folder and stuff');
-		
+
 		if (isset($composer_obj->orange->permission)) {
-			
+
 			$permissions = $composer_obj->orange->permission;
-		
+
 			if (is_array($permissions)) {
 				foreach ($permissions[0] as $filename=>$filemode) {
-		
+
 					if (substr($filename,0,1) !== '#') {
 						ci()->console->output('<yellow>'.$filename.' <cyan>'.$filemode);
-					
+
 						/* does this folder exist? */
 						if (!file_exists(ROOTPATH.'/'.$filename)) {
 							@mkdir(ROOTPATH.'/'.$filename, octdec($filemode), true);
 						}
-						
+
 						$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(ROOTPATH.'/'.$filename));
-						
+
 						foreach($iterator as $item) {
 					    @chmod($item, octdec($filemode));
 						}
@@ -152,14 +160,14 @@ class Orange_tools {
 		ci()->console->output('Relink Symbolic Links');
 
 		if (isset($composer_obj->orange->symlink)) {
-			
+
 			$links = $composer_obj->orange->symlink;
 
 			if (is_array($links)) {
 				foreach ($links[0] as $public => $private) {
 					if (substr($public,0,1) !== '#') {
 						ci()->console->output('<yellow>'.$private.' <white>>> <cyan>'.$public);
-					
+
 						$this->relative_symlink($private, $public);
 					}
 				}
@@ -186,7 +194,7 @@ class Orange_tools {
 		/* create it */
 		return symlink(ROOTPATH . $target, ROOTPATH . $link);
 	}
-	
+
 	protected function get_composer_object($composer_file) {
 		ci()->console->output('Using Composer File <yellow>'.$composer_file);
 
@@ -195,18 +203,18 @@ class Orange_tools {
 
 			if ($composer_obj === null) {
 				ci()->console->error('composer.json malformed');
-				
+
 				exit(1);
 			}
 		} else {
 			ci()->console->error('can not locate composer.json as "'.$composer_file.'"');
-			
+
 			exit(1);
 		}
-		
+
 		return $composer_obj;
 	}
-	
+
 	public function site_down() {
 		$data = [
 			'group'=>'application',
@@ -214,12 +222,12 @@ class Orange_tools {
 			'value'=>0,
 			'enabled'=>1,
 		];
-	
+
 		ci('o_setting_model')->update_if_exists($data,['group'=>'application','name'=>'site open']);
-		
-		
+
+
 	}
-	
+
 	public function site_up() {
 		$data = [
 			'group'=>'application',
@@ -227,7 +235,7 @@ class Orange_tools {
 			'value'=>1,
 			'enabled'=>1,
 		];
-	
+
 		ci('o_setting_model')->update_if_exists($data,['group'=>'application','name'=>'site open']);
 	}
 
