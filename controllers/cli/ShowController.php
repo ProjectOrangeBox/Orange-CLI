9806338774bf9796629e2889367677c3e743afda
Don Myers <donmyers@quadratec.com>
2018-07-24 18:54:14 -0400

    add mutil-line comments

--- controllers/cli/ShowController.php
+++ controllers/cli/ShowController.php
@@ -7,7 +7,7 @@ class ShowController extends MY_Controller {
 	*/
 	public function validateCliAction($arg=null) {
 		$orange_paths = orange_paths('classes');
-		
+
 		foreach ($orange_paths as $name=>$path) {
 			if (substr($name,0,strlen('validate_')) == 'validate_') {
 				$this->line($name,$path,$arg);
@@ -20,7 +20,7 @@ class ShowController extends MY_Controller {
 	*/
 	public function pearCliAction($arg=null) {
 		$orange_paths = orange_paths('classes');
-		
+
 		foreach ($orange_paths as $name=>$path) {
 			if (substr($name,0,strlen('pear_')) == 'pear_') {
 				$this->line($name,$path,$arg);
@@ -33,7 +33,7 @@ class ShowController extends MY_Controller {
 	*/
 	public function filterCliAction($arg=null) {
 		$orange_paths = orange_paths('classes');
-		
+
 		foreach ($orange_paths as $name=>$path) {
 			if (substr($name,0,strlen('filter_')) == 'filter_') {
 				$this->line($name,$path,$arg);
@@ -46,7 +46,7 @@ class ShowController extends MY_Controller {
 	*/
 	public function modelsCliAction($arg=null) {
 		$orange_paths = orange_paths('models');
-		
+
 		foreach ($orange_paths as $name=>$path) {
 			$this->line($name,$path,$arg);
 		}
@@ -57,31 +57,47 @@ class ShowController extends MY_Controller {
 	*/
 	public function librariesCliAction($arg=null) {
 		$orange_paths = orange_paths('libraries');
-		
+
 		foreach ($orange_paths as $name=>$path) {
 			$this->line($name,$path,$arg);
 		}
 	}
-	
+
 	protected function line($name,$path,$arg) {
-		echo str_pad($name, 40);
-		
+		$padding = 40;
+
+		echo str_pad($name,$padding);
+
 		if ($arg == '_p') {
 			/* show path */
-			echo $path;
+			echo $path.PHP_EOL;
 		} else {
-			echo $this->get_help($path);
+			echo $this->get_help($path,$padding);
 		}
-		
-		echo chr(10);
 	}
-	
-	protected function get_help($filepath) {
+
+	protected function get_help($filepath,$padding) {
 		$content = file_get_contents($filepath);
-		
+
 		$int = preg_match_all('/@show(.*)/m', $content, $matches, PREG_SET_ORDER, 0);
-		
-		return ($int) ? trim($matches[0][1]) : '';
+
+		$text = '';
+
+		if ($int) {
+			$idx = 1;
+
+			foreach ($matches as $match) {
+				if ($idx == 1) {
+					$text .= trim($match[1]).PHP_EOL;
+				} else {
+					$text .= str_repeat(' ',$padding).trim($match[1]).PHP_EOL;
+				}
+
+				$idx++;
+			}
+		}
+
+		return trim($text).PHP_EOL;
 	}
 
 }
