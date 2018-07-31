66af8ec4ab665c5e271fb4eb452b4a29541519ae
Don Myers <donmyers@quadratec.com>
2018-07-31 08:49:12 -0400

    add migration to deploy helper

--- controllers/cli/Deploy_helperController.php
+++ controllers/cli/Deploy_helperController.php
@@ -6,15 +6,20 @@ class Deploy_helperController extends MY_Controller {
 		Generate the Deploy JSON for adding all found GIT Repositories
 	*/
 	public function indexCliAction() {
-		/*
-		"gitx update {PWD}/packages/projectorangebox/extra-validations {GITBRANCH}"
-		*/
-
 		$autoload = load_config('autoload','autoload');
+		$packages = $autoload['packages'];
 
-		foreach ($autoload['packages'] as $package) {
-			echo '"gitx update {PWD}'.str_replace(ROOTPATH,'',$package).' {GITBRANCH}",'.chr(10);
+		$packages = array_merge([''],$packages);
+
+		foreach ($packages as $package) {
+			/* "gitx update {PWD}/packages/projectorangebox/extra-validations {GITBRANCH}" */
+			$git_update[] = '"gitx update {PWD}'.str_replace(ROOTPATH,'',$package).' {GITBRANCH}"';
+
+			/* "cd {PWD}/public;php index.php cli/migrate/up packages/projectorangebox/scaffolding" 	*/
+			$migrations[] = '"cd {PWD}/public;php index.php cli/migrate/up'.str_replace(ROOTPATH,' ',$package).'"';
 		}
+
+		echo implode(','.PHP_EOL,$git_update).PHP_EOL.PHP_EOL.implode(','.PHP_EOL,$migrations).PHP_EOL;
 
 	}
 }
