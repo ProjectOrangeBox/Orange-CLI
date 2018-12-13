286f55b0dfa8b81270d409a3334d6f9c31bfe5c4
Don Myers <donmyers@icloud.com>
2018-12-13 14:37:41 -0500

    update deploy helper

--- controllers/cli/Deploy_helperController.php
+++ controllers/cli/Deploy_helperController.php
@@ -1,11 +1,13 @@
 <?php
 
-class Deploy_helperController extends MY_Controller {
+class Deploy_helperController extends MY_Controller
+{
 
 	/**
 		Generate the Deploy JSON for adding all found GIT Repositories
-	*/
-	public function indexCliAction() {
+	 */
+	public function indexCliAction()
+	{
 		$console = new League\CLImate\CLImate;
 
 		$autoload = load_config('autoload','autoload');
@@ -19,6 +21,9 @@ class Deploy_helperController extends MY_Controller {
 
 			/* "cd {PWD}/public;php index.php cli/migrate/up packages/projectorangebox/scaffolding" 	*/
 			$migrations[] = '"cd {PWD}/public;php index.php cli/migrate/up'.str_replace(ROOTPATH,' ',$package).'"';
+
+			/* gitx checkout https://github.com/ProjectOrangeBox/Orange_v2_cli.git {PWD}/packages/projectorangebox/migrations {GITBRANCH} */
+			$checkout[] = '"gitx checkout '.$this->get_remote($package).' {PWD}'.str_replace(ROOTPATH,'',$package).' {GITBRANCH}"';
 		}
 
 		$console->border();
@@ -26,10 +31,35 @@ class Deploy_helperController extends MY_Controller {
 		$console->out(implode(','.PHP_EOL,$git_update));
 
 		$console->border();
-		
+
 		$console->out(implode(','.PHP_EOL,$migrations));
 
 		$console->border();
 
+		$console->out(implode(','.PHP_EOL,$checkout));
+
+		$console->border();
 	}
+
+	protected function get_remote($package)
+	{
+		echo '.';
+
+		$package = (empty($package)) ? ROOTPATH : $package;
+
+		$cli = 'cd '.$package.';git remote show origin';
+
+		exec($cli,$output,$return_var);
+
+		$fetch = trim($output[1]);
+
+		$line = '';
+
+		if (substr($fetch,0,5) == 'Fetch') {
+			$line = substr($fetch,11);
+		}
+
+		return $line;
+	}
+
 }
