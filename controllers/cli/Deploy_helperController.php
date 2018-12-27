9cbd43e582dac02943d7174a21954f2fdf0a5d15
Don Myers <donmyers@icloud.com>
2018-12-27 11:09:06 -0500

    updates for deployer cli

--- controllers/cli/Deploy_helperController.php
+++ controllers/cli/Deploy_helperController.php
@@ -10,57 +10,58 @@ class Deploy_helperController extends MY_Controller
 	{
 		$console = new League\CLImate\CLImate;
 
+		$console->Blue()->out('Inspecting Packages');
+
 		$autoload = load_config('autoload','autoload');
 		$packages = $autoload['packages'];
-		
+
 		/* this adds root application folder */
 		$packages = array_merge([''],$packages);
 
-		foreach ($packages as $package) {
+		$progress = $console->progress()->total(count($packages));
+
+		foreach ($packages as $idx=>$package) {
+			$progress->current($idx+1);
+
 			/* "gitx update {PWD}/packages/projectorangebox/extra-validations {GITBRANCH}" */
 			$git_update[] = '"gitx update {PWD}'.str_replace(ROOTPATH,'',$package).' {GITBRANCH}"';
 
 			/* "cd {PWD}/public;php index.php cli/migrate/up packages/projectorangebox/scaffolding" 	*/
 			$migrations[] = '"cd {PWD}/public;php index.php cli/migrate/up'.str_replace(ROOTPATH,' ',$package).'"';
 
-			/* gitx checkout https://github.com/ProjectOrangeBox/Orange_v2_cli.git {PWD}/packages/projectorangebox/migrations {GITBRANCH} */
-			$checkout[] = '"gitx checkout '.$this->get_remote($package,$console).' {PWD}'.str_replace(ROOTPATH,'',$package).' {GITBRANCH}"';
+			if ($remote = $this->get_remote($package)) {
+				/* gitx checkout https://github.com/ProjectOrangeBox/Orange_v2_cli.git {PWD}/packages/projectorangebox/migrations {GITBRANCH} */
+				$checkout[] = '"gitx checkout '.$remote.' {PWD}'.str_replace(ROOTPATH,'',$package).' {GITBRANCH}"';
+			}
 		}
 
-		$console->border();
-
+		$console->br()->Blue()->out('Deploy commands to update packages - copy and paste as needed.');
 		$console->out(implode(','.PHP_EOL,$git_update));
 
-		$console->border();
-
+		$console->br()->Blue()->out('Deploy commands to migrate packages up - copy and paste as needed.');
 		$console->out(implode(','.PHP_EOL,$migrations));
 
-		$console->border();
-
+		$console->br()->Blue()->out('Deploy commands to checkout packages - copy and paste as needed.');
 		$console->out(implode(','.PHP_EOL,$checkout));
-
-		$console->border();
 	}
 
-	protected function get_remote($package,$console)
+	protected function get_remote($package)
 	{
-		$console->out('.');
-
 		$package = (empty($package)) ? ROOTPATH : $package;
 
-		$cli = 'cd '.$package.';git remote show origin';
+		if (file_exists($package.'/.git')) {
+			exec('cd '.$package.';git remote show origin',$output,$return_var);
 
-		exec($cli,$output,$return_var);
+			foreach ($output as $line) {
+				$line = trim($line);
 
-		$fetch = trim($output[1]);
-
-		$line = '';
-
-		if (substr($fetch,0,5) == 'Fetch') {
-			$line = substr($fetch,11);
+				if (substr(strtolower($line),0,11) == 'fetch url: ') {
+					return trim(substr($line,11));
+				}
+			}
 		}
 
-		return $line;
+		return false;
 	}
 
 }
