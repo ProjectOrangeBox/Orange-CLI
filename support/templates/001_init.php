1f01547e20bd9d08c3d3f06bf73edc81f01fb80d
Don Myers <donmyers@icloud.com>
2019-01-15 11:21:34 -0500

    update template

--- support/templates/001_init.php
+++ support/templates/001_init.php
@@ -2,14 +2,12 @@
 
 class Migration_001_init extends Migration_base {
 
-	/* example up function */
 	public function up() {
 		echo $this->migration('up');
 		
 		return true;
 	}
 
-	/* example down function */
 	public function down() {
 		echo $this->migration('down');
 		
